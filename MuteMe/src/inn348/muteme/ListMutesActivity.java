3da8c5ae1dea0a0cf34e88eb1a934b691ae4b28f
Ramona <chong.lu@uqconnect.edu.au>
2014-08-16 18:27:10 +1000

    Added basic mutes list

--- MuteMe/src/inn348/muteme/ListMutesActivity.java
+++ MuteMe/src/inn348/muteme/ListMutesActivity.java
@@ -2,8 +2,9 @@ package inn348.muteme;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ListMutesActivity extends Activity {
 
@@ -12,6 +13,10 @@ public class ListMutesActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_mutes);
 		
+		ListView muteListView = (ListView)findViewById(R.id.muteListView);
+		String[] muteListData = new String[]{"Time and Location1","Time1","Location1"};
+		ListAdapter mutesAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, muteListData);
 		
+		muteListView.setAdapter(mutesAdapter);
 	}
 }
